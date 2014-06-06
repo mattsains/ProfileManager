908da3bf2765f38bf9d6cc67394471048b843952
Matthew Sainsbury <matthew@sainsbury.za.net>
2014-06-06 10:45:35 +0200

    Initial commit

---
+++ gen/net/za/sainsbury/profilemanager/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.za.sainsbury.profilemanager;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/net/za/sainsbury/profilemanager/ProfileChangeIntent.java
@@ -0,0 +1,20 @@
+package net.za.sainsbury.profilemanager;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class ProfileChangeIntent extends  IntentService {
+
+
+	public ProfileChangeIntent(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent workIntent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
